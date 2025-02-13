9029e2c37d55a597fab643f821612b2db40f99e9
KEMMOUNE Houda <113966070+HOUDA-KEMMOUNE@users.noreply.github.com>
2025-02-13 20:56:26 +0000

    last_word

--- last_word.c
+++ last_word.c
@@ -1,59 +1,33 @@
-#include <stdio.h>
 #include <unistd.h>
-
-int check_space(char *str)
-{
-    int i;
-
-    i = 0;
-    while (str[i])
-    {
-        if (str[i] == ' ' || str[i] == '\t')
-            return (0);
-        i++;
-    }
-    return (1);
-}
 
 int main(int ac, char **av)
 {
     if (ac == 2)
     {
-        int i;
-        int end;
-        int start;
-        // int space;
+        int     i;
+        int     end;
+        char    *last_word;
 
         i = 0;
-        if (check_space(av[1]) == 1)
-        {
-            while (av[1][i])
-            {
-                write(1, &av[1][i], 1);
-                i++;
-            }
-            return (0);
-        }
-        while(av[1][i])
+        while (av[1][i])
             i++;
-        printf("i --> %d\n", i);
         end = i - 1;
-        printf("end = i - 1 --> %d\n", end);
-        while ((end >= 0))
+        while (av[1][end])
         {
-            // if ((av[1][end] == ' ' || av[1][end] == '\t'))
-            //     end--;
-            if ((av[1][end] != ' ' || av[1][end] != '\t') && (av[1][end--] == ' ' || av[1][end--] == '\t'))
+            if (av[1][end] > 32 && av[1][end - 1] <= 32)
+            {
+                last_word = &av[1][end];
                 break ;
+            }
             end--;
         }
-            // end--;
-        printf("%d\n", end);
-        start = end + 1;
-        while (av[1][start])
+        i = 0;
+        while (last_word && last_word[i] > 32)
         {
-            write(1, &av[1][start], 1);
-            start++;
+            if (last_word[i] <= 32)
+                break ;
+            write(1, &last_word[i], 1);
+            i++;
         }
     }
     write(1, "\n", 1);
