a833f517365ae33046ca4b2e06085ead0fa24e45
KEMMOUNE Houda <113966070+HOUDA-KEMMOUNE@users.noreply.github.com>
2025-02-21 11:50:17 +0000

    last update -> finish !

--- station_counter/index.js
+++ station_counter/index.js
@@ -11,6 +11,9 @@ let refresh;
 let butt1_ret;
 let butt2_ret;
 let butt3_ret;
+let count1;
+let count2;
+let count3;
 
 count = 0;
 count_el = document.getElementById("count_el");
@@ -20,44 +23,47 @@ text3 = document.getElementById("text3");
 button1 = document.getElementById("button1");
 button2 = document.getElementById("button2");
 button3 = document.getElementById("button3");
-done = document.getElementById("done");
 refresh = document.getElementById("refresh");
+count1 = document.getElementById("count1");
+count2 = document.getElementById("count2");
+count3 = document.getElementById("count3");
 
+let first = 0;
 function ft_firstButt() {
-  count_el.innerText = count;
   count++;
+  first++;
+  count_el.innerText = count;
+  count1.innerText = first;
   return count;
 }
-butt1_ret = ft_firstButt();
 
+let second = 0;
 function ft_secondButt() {
-  count_el.innerText = count;
   count++;
+  second++;
+  count_el.innerText = count;
+  count2.innerText = second;
   return count;
 }
-butt2_ret = ft_secondButt();
 
+let third = 0;
 function ft_thirdButt() {
-  count_el.innerText = count;
   count++;
+  third++;
+  count_el.innerText = count;
+  count3.innerText = third;
   return count;
-}
-butt3_ret = ft_thirdButt();
-
-function ft_doneButt() {
-  ft_refreshButt();
-  text1.innerText += butt1_ret + " سبحان اللّه";
-  text2.innerText += butt2_ret + " اللّه أكبر";
-  text3.innerText += butt3_ret + " لا إله إلا اللّه";
 }
 
 function ft_refreshButt() {
   count = 0;
+  first = 0;
+  second = 0;
+  third = 0;
   count_el.innerText = count;
-  console.log(count);
-  text1.innerText = "You done:";
-  text2.innerText = "You done:";
-  text3.innerText = "You done:";
+  count1.innerText = first;
+  count2.innerText = second;
+  count3.innerText = third;
 }
 
 refresh.onclick = ft_refreshButt;
@@ -65,4 +71,3 @@ button1.onclick = ft_firstButt;
 button2.onclick = ft_secondButt;
 button3.onclick = ft_thirdButt;
 button1.onclick = ft_firstButt;
-done.onclick = ft_doneButt;
